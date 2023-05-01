6af256a0e9c03eea26d1e3bb602b9c5090a8aec4
kana.hashimoto <kana.hashimoto@j-q.co.jp>
2023-05-01 19:31:53 +0900

    getAllTasksを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,15 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT t FROM Task AS t ORDER BY t.id DESC"
+    )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Integer id;
 
     @Column(name = "content", length = 255, nullable = false)
